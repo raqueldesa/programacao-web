62b4226b1a23daf763d203c742ac810c179b82da
Raquel de Sá Silva <99363543+raqueldesa@users.noreply.github.com>
2024-05-15 17:31:23 -0400

    Create index.js

---
+++ js1/index.js
@@ -0,0 +1,58 @@
+
+
+for(let i = 1; i <= 10; i++){
+    //document.write(`<h1>${i}</h1>`)
+    document.write(`<table > 
+    <thead >
+        <th class="head" colspan="2">Produtos de ${i}</th>
+        
+    </thead>
+    <tbody>
+    
+        <tr>
+          <td align="center" class="aa">${i} x 1</td>
+          <td align="center">${i * 1}</td>
+        </tr>
+        <tr>
+          <td align="center" class="aa">${i} x 2</td>
+          <td align="center">${i * 2}</td>
+        </tr>
+        <tr>
+          <td align="center" class="aa">${i} x 3</td>
+          <td align="center">${i * 3}</td>
+        </tr>
+        <tr>
+          <td align="center" class="aa">${i} x 4</td>
+          <td align="center">${i * 4}</td>
+        </tr>
+        <tr>
+          <td align="center" class="aa">${i} x 5</td>
+          <td align="center">${i * 5}</td>
+        </tr>
+        <tr>
+          <td align="center" class="aa">${i} x 6</td>
+          <td align="center">${i * 6}</td>
+        </tr>
+        <tr>
+          <td align="center" class="aa">${i} x 7</td>
+          <td align="center">${i * 7}</td>
+        </tr>
+        <tr>
+          <td align="center" class="aa">${i} x 8</td>
+          <td align="center">${i * 8}</td>
+        </tr>
+        <tr>
+          <td align="center" class="aa">${i} x 9</td>
+          <td align="center">${i * 9}</td>
+        </tr>
+        <tr>
+          <td align="center" class="aa">${i} x 10</td>
+          <td align="center">${i * 10}</td>
+        </tr>
+    
+
+
+    </tbody>
+</table>
+    `)
+}
